1749794573b28f47aac1eb35634e9c3dfeda964f
Frederik Feldmeier <74424903+elfeldos@users.noreply.github.com>
2023-10-22 00:04:53 +0200

    parsing rss feed into xml file and adding uid

--- main.py
+++ main.py
@@ -15,3 +15,78 @@ def loadRSS():
 
     with open("lebensmittelwarnungen.xml", "wb") as f:
         f.write(response.content)
+
+def parseXML(xmlfile):
+    # create element tree object
+    tree = ET.parse(xmlfile)
+
+    # get root element
+    root = tree.getroot()
+
+    # creating empty list for issue items
+    foodissues = []
+
+    for item in root.findall('./channel/item'):
+
+        # emtpy issues dictionary
+        issues = {}
+
+        # iterate child elements of issue
+        for child in item:
+            
+             # special checking for namespace object content:media
+            if child.tag == '{http://search.yahoo.com/mrss/}content':
+                issues['media'] = child.attrib['url']
+            else:
+                issues[child.tag] = child.text.encode('utf8')
+        
+        # append news dictionary to news items list
+        foodissues.append(issues)
+
+    i = 0
+    for item in foodissues:
+        item['ID'] = i
+        i += 1
+    
+    return foodissues
+
+# def addID(foodissues):
+#     for item in foodissues:
+#         item.update({'ID': 0})
+
+#     print(foodissues)
+
+def savetoCSV(foodissues, filename):
+    # specifying the fields of csv file
+    fields = ['guid', 'title', 'pubDate', 'description', 'link', 'content', 'ID']
+
+    # writing to csv file
+    with open(filename, 'w') as csvfile:
+        #creating a new csv dict writer object
+        writer = csv.DictWriter(csvfile, fieldnames = fields)
+
+        # writing headers
+        writer.writeheader()
+
+        # write data rows
+        writer.writerow(foodissues)
+
+
+
+
+def main(): 
+    # load rss from web to update existing xml file 
+    loadRSS() 
+  
+    # parse xml file 
+    foodissues = parseXML('lebensmittelwarnungen.xml')
+    print(foodissues)
+  
+    # store news items in a csv file 
+    savetoCSV(foodissues, 'lebensmittelwarnungen.csv') 
+      
+      
+if __name__ == "__main__": 
+  
+    # calling main function 
+    main() 
